cbebc14409c07e39cfc6dc233664c8e357549b7a
kdKrzysztof <57269917+kdKrzysztof@users.noreply.github.com>
2025-03-01 13:48:41 +0100

    feat: add note creation

---
+++ MauiApp1-noteapp/NotePage.xaml.cs
@@ -0,0 +1,41 @@
+namespace MauiApp1_noteapp;
+
+public partial class NotePage : ContentPage
+{
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+	public NotePage()
+	{
+		InitializeComponent();
+
+		if (File.Exists(_fileName))
+			TextEditor.Text = File.ReadAllText(_fileName);
+
+		viewNote();
+    }
+
+	private void onSaveButtonClicked(object Sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, TextEditor.Text);
+		viewNote();
+
+    }
+
+	private void onDeleteButtonClicked(object Sender, EventArgs e)
+	{
+		if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+		TextEditor.Text = "";
+		viewNote();
+
+    }
+
+	private void viewNote()
+	{
+		if (File.Exists(_fileName))
+			viewNoteContent.Text = File.ReadAllText(_fileName);
+		else
+			viewNoteContent.Text = "";
+    }
+}
