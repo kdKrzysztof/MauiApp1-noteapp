096a37b0f3db08a96a244755720aed234f9331b4
kdKrzysztof <57269917+kdKrzysztof@users.noreply.github.com>
2025-03-01 14:13:38 +0100

    Feat: add About and Note Models

---
+++ MauiApp1-noteapp/Models/AboutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1_noteapp.Models
+{
+    internal class AboutModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    }
+}
